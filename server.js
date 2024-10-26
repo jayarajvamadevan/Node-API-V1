944e8397c67892e76f7544afaaab275eb1589b30
Jayaraj V.S <jayarajvs@MacBook-Pro.local>
2024-10-26 20:57:49 +0530

    initial project

---
+++ server.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+//routes
+app.get('/',(req,res)=>{
+    res.send("Hello Node API, Looking fine !");
+})
+app.listen (3000,()=>{
+    console.log('Node app is running on port 3000');
+}) 
